57cf671403a35efe92046bde62cc5903a973bf7d
Pablo Hurtado <hurtado.pani@gmail.com>
2014-11-14 18:43:21 +0100

    Almost complete simple case, but missing PROCESS in all the definition!

--- iftpt.py
+++ iftpt.py
@@ -1,35 +1,41 @@
-from pyparsing import Word, alphas, alphanums, dictOf, Literal, restOfLine, OneOrMore, ZeroOrMore, empty, Suppress, replaceWith, Keyword, Group, Combine, Dict, delimitedList, Optional
+from pyparsing import Word, alphas, alphanums, dictOf, Literal, restOfLine, OneOrMore, ZeroOrMore, empty, Suppress, replaceWith, Keyword, Group, Combine, Dict, delimitedList, Optional, CaselessKeyword
 
 def parse():
     identifier = Word(alphas+"_",alphanums+"_")
     # Semicolon as end of line
     eol = Suppress(";")
     # test <name>; (NON-STANDARD IF)
-    testdecl = Suppress(Keyword("test") + identifier + eol)
+    testdecl = Suppress(CaselessKeyword("test") + identifier + eol)
     # endtest; (NON-STANDARD IF)
-    endtest = Suppress(Keyword("endtest") + eol)
+    endtest = Suppress(CaselessKeyword("endtest") + eol)
     # state <name>;
-    statedecl = Keyword("state") + \
+    statedecl = CaselessKeyword("state") + \
             identifier.setResultsName("statename1") + \
             eol
     # endstate;
-    endstate = Keyword("endstate") + eol
+    endstate = CaselessKeyword("endstate") + eol
     # nextstate <name>;
-    nextstate = Keyword("nextstate") + \
+    nextstate = CaselessKeyword("nextstate") + \
             identifier.setResultsName("statename2") + \
             eol
     # exit;
-    ifexit = Keyword("exit") + eol
+    ifexit = CaselessKeyword("exit") + eol
     # input <name>(params...);
-    ifinput= Keyword("input") + \
-            (identifier + 
-                    Suppress("(") + 
-                    Group(ZeroOrMore(identifier + Optional(Suppress(",")))) + 
+    ifinput= CaselessKeyword("input") + \
+            (identifier +
+                    Suppress("(") +
+                    Group(ZeroOrMore(identifier + Optional(Suppress(",")))) +
                     Suppress(")")
             )\
             .setResultsName("input",listAllMatches=True) + eol
     # output <name>(params...);
-    ifoutput= Keyword("output") + identifier.setResultsName("output",listAllMatches=True) + eol
+    ifoutput= CaselessKeyword("output") + \
+            (identifier +
+                    Suppress("(") +
+                    Group(ZeroOrMore(identifier + Optional(Suppress(",")))) +
+                    Suppress(")")
+            )\
+            .setResultsName("output",listAllMatches=True) + eol
     # full state block
     state = statedecl + \
             OneOrMore(nextstate | ifexit | ifinput | ifoutput ) + \
@@ -50,16 +56,19 @@ def getParams(paramlst):
             ret += p + ","
         ret = ret[0:-1] + '}"'
         return ret
-    else: 
+    else:
         return "NULL"
 
 if __name__ == '__main__':
     parsed = parse()
-    print repr(parsed)
+    #print repr(parsed)
     #print parsed
 
     testn=0
     ordtestn=0
+    signalid=0
+
+    to_write = ""
 
     for elst in parsed:
         testn = testn + 1
@@ -74,22 +83,31 @@ if __name__ == '__main__':
                 pass #TODO
             target = el[el.index('nextstate') + 1]
             nsig = el.count("input") + el.count("output")
-            print """
+            to_write += """
                 purposes[{testn}].numSignals = {nsig};
+                purposes[{testn}].numSignals = {{{processname}}}0;
                 purposes[{testn}].source = "{source}";
                 purposes[{testn}].target = "{target}";
-            """.format(nsig=nsig,testn=testn,source=source,target=target)
+            """.format(nsig=nsig,processname="process",testn=testn-1,source=source,target=target) #TODO FALTA PROCESS!!!!
+
+            signalindex=0
             for inp in selst.input:
                 params = getParams(inp[1]) #TODO Recursividad
-                print """
+                to_write+= """
                     signalData signal{signalid} = {{"{inp}","input",{params}}};
-                """.format(signalid=1,inp=inp[0],params=params) #TODO signalid
+                    purposes[{testn}].signals[{signalindex}] = signal{signalid};
+                """.format(testn=testn-1,signalindex=signalindex,signalid=signalid,inp=inp[0],params=params)
+                signalid+=1
+                signalindex+=1
             for outp in selst.output:
-                print """
-                    signalData signal{signalid} = {{"{outp}","output",NULL}};
-                """.format(signalid=2,outp=outp)
+                params = getParams(outp[1]) #TODO Recursividad
+                to_write+= """
+                    signalData signal{signalid} = {{"{outp}","output",{params}}};
+                    purposes[{testn}].signals[{signalindex}] = signal{signalid};
+                """.format(testn=testn-1,signalindex=signalindex,signalid=signalid,outp=outp[0],params=params)
+                signalid+=1
+                signalindex+=1
 
-            
     f = open('test.C','w')
 
     cstr = '''
@@ -98,12 +116,12 @@ if __name__ == '__main__':
         numPurposes = {1};
 
         int i;
-            for (i=0; i < numPurposes;i++){{ 
+        for (i=0; i < numPurposes;i++){{
             purposes[i].status = false;
-            purposes[i].visited = false;      	
-            purposes[i].process = NULL;  
-            purposes[i].source = NULL;      	
-            purposes[i].target = NULL;  			
+            purposes[i].visited = false;
+            purposes[i].process = NULL;
+            purposes[i].source = NULL;
+            purposes[i].target = NULL;
             purposes[i].numBoundClocks = 0;
             purposes[i].numActiveClocks = 0;
             purposes[i].numSignals = 0;
@@ -111,12 +129,12 @@ if __name__ == '__main__':
             purposes[i].depth = -1;
         }}
 
-        for (i=0; i < numOrdPurposes;i++){{ 
+        for (i=0; i < numOrdPurposes;i++){{
             ordPurposes[i].status = false;
             ordPurposes[i].visited = false;
-            ordPurposes[i].process = NULL;  
-            ordPurposes[i].source = NULL;      	
-            ordPurposes[i].target = NULL;      	
+            ordPurposes[i].process = NULL;
+            ordPurposes[i].source = NULL;
+            ordPurposes[i].target = NULL;
             ordPurposes[i].numBoundClocks = 0;
             ordPurposes[i].numActiveClocks = 0;
             ordPurposes[i].numSignals = 0;
@@ -124,6 +142,6 @@ if __name__ == '__main__':
             ordPurposes[i].depth = -1;
         }}
 
-    '''.format(ordtestn, testn)
+    '''.format(ordtestn, testn) + to_write + "\n"
     f.write(cstr)
     f.close()
